34f3cb1d7ec3ab6fa05466becc40359cc87fe65c
Preeti P Yelsangikar <39264359+preetipy@users.noreply.github.com>
2020-10-14 00:58:14 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require("express")
 const app = express()
 
 // use the express-static middleware
-app.use(express.static("public"))
+app.use(express.static("public"));
+app.set('view engine','html')
 
 // define the first route
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
+  res.render("index")
 })
 
 // start the server listening for requests
